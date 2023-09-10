6bd8cb07d062009f1e78dc9341286e3f0b102677
tardelliaraujo <tardelli.blues@gmail.com>
2023-09-10 17:38:16 -0300

    content-created

---
+++ src/app/data/dataFakes.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVOS ANTIGOS HERÓIS DA COMICS",
+        "description": "Comics anuncia volta de heróis mais antigos.",
+        "photoCover": "https://thenerdz.com.br/wp-content/uploads/2023/01/Image-Comics-Line-Up-1.webp"
+    },
+    {
+        "id": "2",
+        "title": "Heróis estão de volta!",
+        "description": "olá",
+        "photoCover": "https://comics.olanerd.com/wp-content/uploads/2023/05/1683058042_Novos-quadrinhos-da-Marvel-esta-semana-3-de-maio-de.jpg"
+
+    }
+    
+]
